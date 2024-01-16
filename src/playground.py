55572b02261f5425a26a1c18ab88b0f9ee7e8225
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-16 20:31:42 +0100

    small refactor

--- src/playground.py
+++ src/playground.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-Array2D = np.ndarray
-Array1D = np.ndarray
+type Array2D = np.ndarray
+type Array1D = np.ndarray
 
 
 def generate_playground() -> Array2D:
@@ -15,7 +15,7 @@ def generate_playground() -> Array2D:
     return playground
 
 
-def get_piece_dest(piece, piece_coordinates, playground):
+def get_piece_dest(playground: Array2D, piece: Array2D, piece_coordinates: Array1D) -> Array2D:
     piece_x, piece_y = piece_coordinates
     dim_y, dim_x = piece.shape
     piece_dest = playground[piece_y:piece_y + dim_y, piece_x: piece_x + dim_x]
@@ -23,13 +23,13 @@ def get_piece_dest(piece, piece_coordinates, playground):
 
 
 def is_valid_position(playground: Array2D, piece: Array2D, piece_coordinates: Array1D) -> bool:
-    piece_dest = get_piece_dest(piece, piece_coordinates, playground)
+    piece_dest = get_piece_dest(playground, piece, piece_coordinates)
 
     return (piece_dest * piece).max() == 0
 
 
 def insert_piece(playground: Array2D, piece: Array2D, piece_coordinates: Array1D) -> None:
-    piece_dest = get_piece_dest(piece, piece_coordinates, playground)
+    piece_dest = get_piece_dest(playground, piece, piece_coordinates)
 
     piece_dest += piece
 
