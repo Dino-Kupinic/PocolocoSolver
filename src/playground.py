b40aa65a539f0c74127bcfabfe5abb004bafe197
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-16 16:46:47 +0100

    refactor insert_piece, extracted piece_dest

--- src/playground.py
+++ src/playground.py
@@ -15,22 +15,23 @@ def generate_playground() -> Array2D:
     return playground
 
 
-def is_valid_position(playground: Array2D, piece: Array2D, piece_coordinates: Array1D) -> bool:
+def get_piece_dest(piece, piece_coordinates, playground):
     piece_x, piece_y = piece_coordinates
     dim_y, dim_x = piece.shape
     piece_dest = playground[piece_y:piece_y + dim_y, piece_x: piece_x + dim_x]
+    return piece_dest
+
+
+def is_valid_position(playground: Array2D, piece: Array2D, piece_coordinates: Array1D) -> bool:
+    piece_dest = get_piece_dest(piece, piece_coordinates, playground)
 
     return (piece_dest * piece).max() == 0
 
 
-def insert_piece(playground, piece, piece_coordinates) -> None:
-    for row_index in range(len(piece)):
-        for col_index in range(len(piece[row_index])):
-            is_not_requested = piece[row_index][col_index] == 0
-            if is_not_requested:
-                continue
+def insert_piece(playground: Array2D, piece: Array2D, piece_coordinates: Array1D) -> None:
+    piece_dest = get_piece_dest(piece, piece_coordinates, playground)
 
-            playground[row_index + piece_coordinates[1]][col_index + piece_coordinates[0]] = piece[row_index][col_index]
+    piece_dest += piece
 
 
 def print_playground(playground) -> None:
