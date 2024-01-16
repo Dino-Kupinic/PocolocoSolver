5b233f27530076ab672c00fee74df13be2fd9f64
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-16 14:43:21 +0100

    extracted main code into playground

---
+++ src/playground.py
@@ -0,0 +1,42 @@
+piece = [
+    [2, 0],
+    [2, 2]
+]
+
+
+def generate_playground():
+    playground = [
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+    ]
+
+    return playground
+
+
+def is_valid_position(playground, piece_coordinates):
+    for row_index in range(len(piece)):
+        for col_index in range(len(piece[row_index])):
+            is_occupied = playground[row_index + piece_coordinates[1]][col_index + piece_coordinates[0]] == 1
+            is_requested = piece[row_index][col_index] == 2
+
+            if is_occupied and is_requested:
+                return False
+
+    return True
+
+
+def insert_piece(playground, piece_coordinates):
+    for row_index in range(len(piece)):
+        for col_index in range(len(piece[row_index])):
+            is_not_requested = piece[row_index][col_index] == 0
+            if is_not_requested:
+                continue
+
+            playground[row_index + piece_coordinates[1]][col_index + piece_coordinates[0]] = piece[row_index][col_index]
+
+
+def print_playground(playground):
+    for row in playground:
+        print(row)
