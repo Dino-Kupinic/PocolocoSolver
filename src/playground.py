21704db87a5d55ae58175b7008534ac007fb1f93
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-16 16:41:31 +0100

    refactored is_valid_position using numpy

--- src/playground.py
+++ src/playground.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def generate_playground() -> list[list[int]]:
     playground = [
         [0, 0, 1, 1],
@@ -9,16 +12,16 @@ def generate_playground() -> list[list[int]]:
     return playground
 
 
-def is_valid_position(playground, piece, piece_coordinates) -> bool:
-    for row_index in range(len(piece)):
-        for col_index in range(len(piece[row_index])):
-            is_occupied = playground[row_index + piece_coordinates[1]][col_index + piece_coordinates[0]] == 1
-            is_requested = piece[row_index][col_index] == 2
+Array2D = np.ndarray
+Array1D = np.ndarray
 
-            if is_occupied and is_requested:
-                return False
 
-    return True
+def is_valid_position(playground: Array2D, piece: Array2D, piece_coordinates: Array1D) -> bool:
+    piece_x, piece_y = piece_coordinates
+    dim_y, dim_x = piece.shape
+    piece_dest = playground[piece_y:piece_y + dim_y, piece_x: piece_x + dim_x]
+
+    return (piece_dest * piece).max() == 0 
 
 
 def insert_piece(playground, piece, piece_coordinates) -> None:
