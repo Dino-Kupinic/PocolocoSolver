d3aeafe571d811ab7b735ae8b4ec96fd4fcdec3f
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-16 16:41:14 +0100

    added test for insert_piece()

--- tests/test_playground.py
+++ tests/test_playground.py
@@ -1,21 +1,23 @@
-from src.playground import is_valid_position
+import numpy as np
+
+from src.playground import is_valid_position, insert_piece
 
 
 def test_is_valid_position_basic():
-    playground1 = [
+    playground1 = np.array([
         [0]
-    ]
+    ])
 
-    playground1_occupied = [
+    playground1_occupied = np.array([
         [1]
-    ]
+    ])
 
-    piece1 = [
+    piece1 = np.array([
         [2]
-    ]
+    ])
 
-    assert is_valid_position(playground1, piece1, [0, 0])
-    assert not is_valid_position(playground1_occupied, piece1, [0, 0])
+    assert is_valid_position(playground1, piece1, np.array([0, 0]))
+    assert not is_valid_position(playground1_occupied, piece1, np.array([0, 0]))
 
 
 def test_is_valid_position_advanced():
@@ -24,10 +26,31 @@ def test_is_valid_position_advanced():
         [0, 0, 0]
     ]
 
-    piece_l = [
+    piece_l = np.array([
         [2, 0],
         [2, 2]
-    ]
+    ])
 
-    assert is_valid_position(playground2, piece_l, [0, 0])
-    assert not is_valid_position(playground2, piece_l, [1, 0])
+    assert is_valid_position(playground2, piece_l, np.array([0, 0]))
+    assert not is_valid_position(playground2, piece_l, np.array([1, 0]))
+
+
+def test_insert_piece_advanced():
+    playground2 = np.array([
+        [0, 1, 0],
+        [0, 0, 0]
+    ])
+
+    piece_l = np.array([
+        [2, 0],
+        [2, 2]
+    ])
+
+    insert_piece(playground2, piece_l, np.array([0, 0]))
+
+    playground2_expected = np.array([
+        [2, 1, 0],
+        [2, 2, 0]
+    ])
+
+    assert (playground2 == playground2_expected).all()
