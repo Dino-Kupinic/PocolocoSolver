30b7481a94add44633eecff62099e0674cb73822
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-23 16:06:40 +0100

    add more tests

--- tests/test_playground.py
+++ tests/test_playground.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from src.playground import is_valid_position, insert_piece
+from src.playground import is_valid_position, insert_piece, remove_piece, get_piece_dest, Array2D, Array1D
 
 
 def test_is_valid_position_basic():
@@ -54,3 +54,42 @@ def test_insert_piece_advanced():
     ])
 
     assert (playground2 == playground2_expected).all()
+
+
+def test_remove_piece():
+    playground3 = np.array([
+        [2, 1, 0],
+        [2, 2, 0]
+    ])
+    piece_l = np.array([
+        [2, 0],
+        [2, 2]
+    ])
+
+    remove_piece(playground3, piece_l, np.array([0, 0]))
+
+    playground3_expected = np.array([
+        [0, 1, 0],
+        [0, 0, 0]
+    ])
+
+    assert (playground3 == playground3_expected).all()
+
+
+def test_get_piece_dest():
+    playground2 = np.array([
+        [0, 1, 0],
+        [0, 0, 0]
+    ])
+
+    piece_l = np.array([
+        [2, 0],
+        [2, 2]
+    ])
+
+    playground2_expected = np.array([
+        [0, 1],
+        [0, 0]
+    ])
+
+    assert (get_piece_dest(playground2, piece_l, np.array([0, 0])) == playground2_expected).all()
