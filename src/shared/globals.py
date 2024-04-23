788feb245c41598e3c17a3e3fd7a99192ce8dd41
mploierMacBook <mploier@htl-steyr.ac.at>
2024-04-23 16:34:24 +0200

    Refactor file types

---
+++ src/shared/globals.py
@@ -0,0 +1,8 @@
+from typing import Final
+
+CB: Final[int] = 9
+"""'Cube Border', Represents the border of the poco loco. Used for "out of bound" checks"""
+OF: Final[int] = 1
+"""'Occupied Field', Represents a field in which a cube resides."""
+EF: Final[int] = 0
+"""'Empty Field', Represents an empty field in which a cube can move into"""
---
+++ src/shared/types.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+type Array2D = np.ndarray
+type Array1D = np.ndarray
