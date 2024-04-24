514cb3ad706f027e11aeee28a8270a23aee82098
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-04-24 16:40:39 +0200

    added list for obstacles in playground

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ from src.playground import generate_playground, is_valid_position, insert_piece,
 
 
 def main():
+    obstacles = ([2, 1, 5], [3, 4, 5], [2, 1, 6], [3, 1, 6], [2, 4, 6], [3, 4, 6],)
     piece_coordinates = np.array([0, 0, 0])
 
     piece_goal = np.array([1, 1, 4])
@@ -29,7 +30,7 @@ def main():
     ])
 
     my_playground = generate_playground()
-    add_obstacles(my_playground, ([2, 1, 5], [2, 1, 6], [3, 1, 6]))
+    add_obstacles(my_playground, obstacles)
     print_playground(my_playground)
     move_piece_through_maze(my_playground, piece_test, piece_coordinates, piece_goal)
 
