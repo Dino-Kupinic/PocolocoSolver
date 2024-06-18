704d26f38a974831b62dbf32ea004ec7b562bbef
mploierMacBook <mploier@htl-steyr.ac.at>
2024-06-18 16:16:24 +0200

    Globalise piece start and end coordinates

--- src/shared/globals.py
+++ src/shared/globals.py
@@ -1,5 +1,7 @@
 from typing import Final
 
+import numpy as np
+
 # Game Field
 
 CB: Final[int] = 9
@@ -11,3 +13,14 @@ EF: Final[int] = 0
 
 # Logging
 log_format: Final[str] = '%(asctime)s - %(message)s'
+
+# Pieces Coordinates
+piece1_start = np.array([0, 0, 0])
+piece2_start = np.array([4, 0, 0])
+piece3_start = np.array([0, 4, 0])
+piece4_start = np.array([4, 4, 0])
+
+piece1_goal = np.array([1, 1, 4])
+piece2_goal = np.array([3, 1, 4])
+piece3_goal = np.array([1, 3, 4])
+piece4_goal = np.array([3, 3, 4])
